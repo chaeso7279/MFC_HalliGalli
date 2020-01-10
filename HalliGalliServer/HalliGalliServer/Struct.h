da9fa4e0c178d72474f988da70efd5919ff7baba
chaeso7279 <chaeso7279@gmail.com>
2020-01-10 16:19:26 +0900

    서버에 카드 구조체 추가

---
+++ HalliGalliServer/HalliGalliServer/Struct.h
@@ -0,0 +1,12 @@
+#ifndef Struct_h__
+#define Struct_h__
+
+enum FRUIT_ID { FRUIT_CHEERY, FRUIT_PEAR, FRUIT_END };
+
+typedef struct tagCard
+{
+	int iFruitID;
+	int iFruitCnt;
+}CARD;
+
+#endif // Struct_h__
